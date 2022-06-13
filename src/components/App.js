c248e691ba81da4a1711dfdb32e22cef372dc3bf
Jacob <jacobmjones17@gmail.com>
2022-06-13 16:39:15 -0400

    commit tree 70ef2c57a89a2471e1dbd6192ad79235ee588b50

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import AdminNavBar from "./AdminNavBar";
 import QuestionForm from "./QuestionForm";
 import QuestionList from "./QuestionList";
@@ -9,9 +9,15 @@ function App() {
   return (
     <main>
       <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm /> : <QuestionList />}
+      {page === "Form" ? <QuestionForm /> : <QuestionList questions={questions}/>}
     </main>
   );
 }
 
 export default App;
+
+
+// //Action Plan:
+// 1) In your QuestionList component we should render QuestionItem component
+// A) using props to pass down state from App component to QuestionList component
+// B) then destructure the props in QuestionItem to render our data in the QuestionItem component
